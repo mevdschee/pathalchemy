0bc906b397f58d01be126fb00c8f7361a1dd4064
Maurits van der Schee <maurits@vdschee.nl>
2019-10-25 01:19:26 +0200

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # path-alchemy
+
+A Python 3 library for querying the database
+
+### Requirements
+
+You need to install the following dependencies:
+
+    sudo apt install libpq-dev
+    pip install psycopg2
+
--- path-alchemy.py
+++ path-alchemy.py
@@ -1,6 +1,14 @@
 import sqlalchemy
+from collections import OrderedDict 
+from hashlib import md5
+from json import JSONEncoder
 
 class PathAlchemy:
+
+    __engine = None
+
+    def __init__(self, dsn):
+        self.__engine = sqlalchemy.create_engine(dsn)
 
     def _get_columns(self, rs):
         columns = []
@@ -9,7 +17,7 @@ class PathAlchemy:
         return columns    
 
     def _get_tables(self, rs, con):
-        tables = {}
+        tables = []
         for column in rs.cursor.description:
             table_name = None
             if hasattr(column, 'table_oid'):
@@ -20,39 +28,119 @@ class PathAlchemy:
                     statement = sqlalchemy.sql.text("""select relname from pg_class where oid=:oid""")
                     table_name = con.execute(statement, {"oid":column.table_oid}).fetchone()[0]
                     table_oids[column.table_oid] = table_name
-            tables[column.name] = table_name
+            tables.append(table_name)
         return tables   
     
-    def _get_path_columns(self, columns, tables):
-        path_columns = []
-        tablecount = len(set(tables.values()))
+    def _get_paths(self, columns, tables):
+        paths = []
+        tablecount = len(set(tables))
         for i, column in enumerate(columns):
             if (column[0:1] != '$'):
                 if tablecount>1:
-                    path_columns.append('$[].' + tables[columns[i]] + '.' + column)
+                    paths.append('$[].' + tables[i] + '.' + column)
                 else:
-                    path_columns.append('$[].' + column)
+                    paths.append('$[].' + column)
             else:
-                path_columns.append(column)
-        return path_columns
+                paths.append(column)
+        return paths
+
+    def _get_meta(self,rs,con):
+        columns = self._get_columns(rs)
+        tables = self._get_tables(rs,con)
+        paths = self._get_paths(columns, tables)
+        meta = []
+        for i, column in enumerate(columns):
+            meta.append({'name':column,'table':tables[i],'path':paths[i]})
+        return meta
 
     def q(self, sql, args):
-        engine = sqlalchemy.create_engine('postgresql+psycopg2://php-crud-api:php-crud-api@127.0.0.1:5432/php-crud-api')
-        with engine.connect() as con:
-
+        with self.__engine.connect() as con:
             statement = sqlalchemy.sql.text(sql)
-
             rs = con.execute(statement, args)
+            meta = self._get_meta(rs,con)
+            records = self._get_all_records(rs, meta)
+            results = self._group_by_separator(records,'[]')
+            results = self._add_hashes(results)
+            results = self._combine_into_tree(results,'.')
+            results = self._remove_hashes(results)
+            print(JSONEncoder().encode(results))
 
-            columns = self._get_columns(rs)
-            tables = self._get_tables(rs,con)
-            path_columns = self._get_path_columns(columns, tables)
-                
-            for row in rs:
-                for i, value in enumerate(row):
-                    print(path_columns[i] + '=' + str(value))
+    def _get_all_records(self, rs, meta):
+        records = []
+        for row in rs:
+            record = OrderedDict()
+            for i, value in enumerate(row):
+                record[meta[i]['path'][1:]] = value
+            records.append(record)
+        return records
 
-p = PathAlchemy()
+    def _group_by_separator(self, records, separator):
+        results = []
+        for record in records:
+            result = OrderedDict()
+            for name,value in record.items():
+                parts = name.split(separator)
+                newName = parts.pop()
+                path = separator.join(parts)
+                if len(parts)>0:
+                    path += separator
+                if not path in result:
+                    result[path] = OrderedDict()
+                result[path][newName] = value
+            results.append(result)
+        return results
+    
+    def _add_hashes(self, records):
+        results = []
+        for record in records:
+            mapping = OrderedDict()
+            for key, part in record.items():
+                hash = md5(JSONEncoder().encode(part).encode('utf-8')).hexdigest()
+                mapping[key] = key[:-2] + '.!' + hash + '!'
+            newKeys = []
+            for key in record.keys():
+                for search in sorted(mapping.keys(), key=len, reverse=True):
+                    key = key.replace(search, mapping[search])
+                newKeys.append(key)
+            results.append(OrderedDict(zip(newKeys, record.values())))
+        return results
+
+    def _combine_into_tree(self, records, separator):
+        results = OrderedDict()
+        for record in records:
+            for name, value in record.items():
+                for key, v in value.items():
+                    path = (name + key).split(separator)
+                    newName = path.pop()
+                    current = results
+                    for p in path:
+                        if not p in current:
+                            current[p] = OrderedDict()
+                        current = current[p]
+                    current[newName] = v
+        return results['']
+
+    def _remove_hashes(self, tree):
+        values = OrderedDict()
+        trees = OrderedDict()
+        results = []
+        for key, value in tree.items():
+            if type(value) == OrderedDict:
+                if key[:1] == '!' and key[-1:] == '!':
+                    results.append(self._remove_hashes(tree[key]))
+                else:
+                    trees[key] = self._remove_hashes(tree[key])
+            else:
+                values[key] = value
+        if len(results):
+            return results
+        return OrderedDict(list(values.items()) + list(trees.items()))
+    
+    @staticmethod
+    def create(username, password, database, address='127.0.0.1', port='5432'):
+        return PathAlchemy('postgresql+psycopg2://'+username+':'+password+'@'+address+':'+port+'/'+database)
+
+p = PathAlchemy.create('php-crud-api','php-crud-api','php-crud-api')
 p.q("""SELECT * from posts where posts.id=:id""",{"id":1})
 print('')
-p.q("""SELECT * from posts,comments where comments.post_id = posts.id and posts.id=:id""",{"id":1})
+p.q("""select posts.id as "$.posts[].id", comments.id as "$.posts[].comments[].id" from posts left join comments on post_id = posts.id where posts.id<=2""",{})
